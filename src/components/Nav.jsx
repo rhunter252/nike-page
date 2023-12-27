d7dc4011b29b4d54609756ccbb71416298e24ca4
Randy Hunter <randyh252@gmail.com>
2023-12-27 16:29:32 -0500

    initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,32 @@
+import { headerLogo } from "../assets/images";
+import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} width={130} height={29} alt="header logo" />
+        </a>
+        <ul className="flex flex-1 justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="hidden max-lg:block">
+          <img src={hamburger} width={30} height={30} alt="hamburger" />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  Footer,
+  SpecialOffers,
+  Subscribe,
+  CustomerReviews,
+  PopularProducts,
+  SuperQuality,
+  Services,
+};
