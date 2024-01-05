073e43d4f1442fe3c240bd81fe892dbbfce3259a
Randy Hunter <randyh252@gmail.com>
2024-01-05 08:40:32 -0500

    made changes to hero, button, and added shoe card

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ label, iconURL }) => {
   return (
     <div>
       <button
-        className="flex justify-center items-center gap-2 px-7 py-4 text-lg leading-none bg-coral-red rounded-full text-white font-montserrat border
+        className="flex justify-center items-center gap-2 px-7 py-4 text-lg leading-none bg-coral-red rounded-full text-white font-montserrat border-coral-red
       "
       >
         {label}
